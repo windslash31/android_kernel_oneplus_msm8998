c052181e9a752274314bb4e0c1ee85d16acb716e
Steve Muckle <smuckle@codeaurora.org>
2013-03-11 16:33:42 -0700

    sched: provide per cpu-cgroup option to notify on migrations
    On systems where CPUs may run asynchronously, task migrations
    between CPUs running at grossly different speeds can cause
    problems.
    
    This change provides a mechanism to notify a subsystem
    in the kernel if a task in a particular cgroup migrates to a
    different CPU. Other subsystems (such as cpufreq) may then
    register for this notifier to take appropriate action when
    such a task is migrated.
    
    The cgroup attribute to set for this behavior is
    "notify_on_migrate" .
    
    Change-Id: Ie1868249e53ef901b89c837fdc33b0ad0c0a4590
    Signed-off-by: Steve Muckle <smuckle@codeaurora.org>
    [rameezmustafa@codeaurora.org: Use new cgroup APIs, fix 64-bit
    			compilation issues and resolve some merge
    			conflicts. Also squash "2bd8075 sched:
    			remove migration notification from RT class"
    			into this patch.]
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
    [joonwoop@codeaurora.org: Incorporated with new __migrate_task().]
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    Signed-off-by: joshuous <joshuous@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7429,6 +7429,8 @@ struct lb_env {
 	struct list_head	tasks;
 };
 
+static DEFINE_PER_CPU(bool, dbs_boost_needed);
+
 /*
  * Is this task likely cache-hot:
  */
@@ -7745,6 +7747,8 @@ static void attach_task(struct rq *rq, struct task_struct *p)
 	activate_task(rq, p, 0);
 	p->on_rq = TASK_ON_RQ_QUEUED;
 	check_preempt_curr(rq, p, 0);
+	if (task_notify_on_migrate(p))
+		per_cpu(dbs_boost_needed, task_cpu(p)) = true;
 }
 
 /*
@@ -9266,7 +9270,11 @@ no_move:
 		}
 	} else {
 		sd->nr_balance_failed = 0;
-
+		if (per_cpu(dbs_boost_needed, this_cpu)) {
+			per_cpu(dbs_boost_needed, this_cpu) = false;
+			atomic_notifier_call_chain(&migration_notifier_head,
+				   this_cpu, (void *)(long)cpu_of(busiest));
+		}
 	}
 	if (likely(!active_balance)) {
 		/* We were unbalanced, so reset the balancing interval */
@@ -9550,6 +9558,12 @@ out_unlock:
 
 	local_irq_enable();
 
+	if (per_cpu(dbs_boost_needed, target_cpu)) {
+		per_cpu(dbs_boost_needed, target_cpu) = false;
+		atomic_notifier_call_chain(&migration_notifier_head,
+					   target_cpu,
+					   (void *)(long)cpu_of(busiest_rq));
+	}
 	return 0;
 }
 
