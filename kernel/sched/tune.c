771955c0490e66ecc336fb811dd8556d04d3e64f
joshuous <joshuous@gmail.com>
2018-07-07 10:26:10 +0200

    WIP: sched/tune: Move boost_slots_init into its own function and create release
    Signed-off-by: joshuous <joshuous@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -748,6 +748,47 @@ dynamic_boost_write(struct cgroup_subsys_state *css, struct cftype *cft,
 
 	return 0;
 }
+
+static void
+boost_slots_init(struct schedtune *st)
+{
+	int i;
+	struct boost_slot *slot;
+
+	/* Initialize boost slots */
+	INIT_LIST_HEAD(&(st->active_boost_slots.list));
+	INIT_LIST_HEAD(&(st->available_boost_slots.list));
+
+	/* Populate available_boost_slots */
+	for (i = 0; i < DYNAMIC_BOOST_SLOTS_COUNT; ++i) {
+		slot = kmalloc(sizeof(*slot), GFP_KERNEL);
+		slot->idx = i;
+		list_add_tail(&(slot->list), &(st->available_boost_slots.list));
+	}
+#define DEBUG
+#ifdef DEBUG
+	list_for_each_entry(slot, &(st->available_boost_slots.list), list) {
+		pr_info("STUNE: initialised: %d\n", slot->idx);
+	}
+#endif
+}
+
+static void
+boost_slots_release(struct schedtune *st)
+{
+	struct boost_slot *slot, *next_slot;
+
+	list_for_each_entry_safe(slot, next_slot, &(st->available_boost_slots.list), list) {
+		list_del(&slot->list);
+		pr_info("STUNE: freed!\n");
+		kfree(slot);
+	}
+	list_for_each_entry_safe(slot, next_slot, &(st->active_boost_slots.list), list) {
+		list_del(&slot->list);
+		pr_info("STUNE: freed!\n");
+		kfree(slot);
+	}
+}
 #endif // CONFIG_DYNAMIC_STUNE_BOOST
 
 static struct cftype files[] = {
@@ -776,10 +817,6 @@ schedtune_boostgroup_init(struct schedtune *st)
 {
 	struct boost_groups *bg;
 	int cpu;
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	int i;
-	struct boost_slot *slot;
-#endif // CONFIG_DYNAMIC_STUNE_BOOST
 
 	/* Keep track of allocated boost groups */
 	allocated_group[st->idx] = st;
@@ -793,23 +830,7 @@ schedtune_boostgroup_init(struct schedtune *st)
 	}
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	/* Initialize boost slots */
-	INIT_LIST_HEAD(&(st->active_boost_slots.list));
-	INIT_LIST_HEAD(&(st->available_boost_slots.list));
-
-	/* Populate available_boost_slots */
-	for (i = 0; i < DYNAMIC_BOOST_SLOTS_COUNT; ++i) {
-		slot = kmalloc(sizeof(*slot), GFP_KERNEL);
-		slot->idx = i;
-		list_add_tail(&(slot->list), &(st->available_boost_slots.list));
-	}
-
-#ifdef DEBUG
-	list_for_each_entry(slot, &(st->available_boost_slots.list), list) {
-		pr_info("STUNE: initialised: %d\n", slot->idx);
-	}
-#endif
-
+	boost_slots_init(st);
 #endif // CONFIG_DYNAMIC_STUNE_BOOST
 
 	return 0;
@@ -860,6 +881,10 @@ out:
 static void
 schedtune_boostgroup_release(struct schedtune *st)
 {
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	/* Free dynamic boost slots */
+	boost_slots_release(st);
+#endif // CONFIG_DYNAMIC_STUNE_BOOST
 	/* Reset this boost group */
 	schedtune_boostgroup_update(st->idx, 0);
 
